0587e043422b53f57a10c417f996f31dcec2d9e1
Com-Pensa <65026007+Com-Pensa@users.noreply.github.com>
2020-05-11 11:11:39 -0300

    Updating the Game C++ file
    Separated functions, middle line and 2 players

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,16 +1,46 @@
 #include "Game.hpp"
 
-//  Builder function
-Game::Game()
+//  Initializer functions
+void Game::init_window()
 {   
-    const int width=16*75,
-              height=9*75;
-    
     this-> mainWindow = new sf::RenderWindow(
-        sf::VideoMode(width, height),
+        sf::VideoMode(1200, 675),
         "Pong SFML",
         sf::Style::Default
     );
+    this-> mainWindow-> setFramerateLimit(40);
+}
+
+void Game::init_middle_line()
+{
+    middleLine.setFillColor(sf::Color::White);
+    middleLine.setPosition(sf::Vector2f(597, 0));
+    middleLine.setSize(sf::Vector2f(5, 675));
+}
+
+void Game::init_players()
+{   
+    this-> rightPlayer = new Player();
+    this-> leftPlayer  = new Player();
+    
+    this-> rightPlayer-> set_position_x(1160);
+    this-> leftPlayer -> set_position_x(20);
+    
+    //  Setting the Keyboard
+    this-> rightPlayer-> set_up_key(sf::Keyboard::Up);
+    this-> rightPlayer-> set_down_key(sf::Keyboard::Down);
+    
+    this-> leftPlayer-> set_up_key(sf::Keyboard::W);
+    this-> leftPlayer-> set_down_key(sf::Keyboard::S);
+}
+
+//  Builder function
+Game::Game()
+{   
+    init_window();
+    init_middle_line();
+
+    init_players();
 }
 
 //  Game functions
@@ -18,18 +48,44 @@ void Game::run()
 {   
     while (this-> mainWindow-> isOpen())
     {   
-        while (this-> mainWindow-> pollEvent(mainWindowEvent))
-        {   
-            switch (mainWindowEvent.type)
-            {   
-                case sf::Event::Closed:
-                    this-> mainWindow-> close();
-                    break;
-            }
-        }
+        process_events();
+        process_live_events();
         
-        this-> mainWindow-> clear(sf::Color::Black);
-        
-        this-> mainWindow-> display();
+        show();
     }
+}
+
+void Game::process_events()
+{   
+    while (this-> mainWindow-> pollEvent(mainWindowEvent))
+    {   
+        switch (mainWindowEvent.type)
+        {   
+            case sf::Event::Closed:
+                this-> mainWindow-> close();
+                break;
+        }
+    }
+}
+
+void Game::process_live_events()
+{   
+    movimentTime=mainClock.getElapsedTime();
+    
+    this-> rightPlayer-> move(movimentTime);
+    this-> leftPlayer -> move(movimentTime);
+    
+    mainClock.restart().asMilliseconds();
+}
+
+void Game::show()
+{   
+    this-> mainWindow-> clear(sf::Color::Black);
+    
+    //  Drawing items
+    this-> mainWindow-> draw(this-> middleLine);
+    this-> mainWindow-> draw(this-> rightPlayer-> get_paddle());
+    this-> mainWindow-> draw(this-> leftPlayer -> get_paddle());
+    
+    this-> mainWindow-> display();
 }
