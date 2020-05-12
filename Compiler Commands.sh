0531f495d9c72c1a89824d083c90b37ad69aee32
Com-Pensa <65026007+Com-Pensa@users.noreply.github.com>
2020-05-12 10:58:13 -0300

    Updating Compiler Commands.sh
    Compiling the Ball.cpp too

--- Compiler Commands.sh
+++ Compiler Commands.sh
@@ -1,5 +1,5 @@
 #! bin/bash
 
-g++ -c src/main.cpp src/Game.cpp src/obj/Player.cpp
-g++ main.o Game.o Player.o -o pong-SFML -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c src/main.cpp src/Game.cpp src/obj/Player.cpp src/obj/Ball.cpp
+g++ main.o Game.o Player.o Ball.o -o pong-SFML -lsfml-graphics -lsfml-window -lsfml-system
 ./pong-SFML
