c0777eb5b20808c0faae22bf104965b5427bd0de
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-05-26 19:28:38 -0300

    Create Player.hpp

---
+++ src/components/obj/Player.hpp
@@ -0,0 +1,26 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Player
+{   
+private:
+    // Player Data and Varibles:
+    sf::RectangleShape  paddle;
+    sf::Keyboard::Key   upKey,
+                        downKey;
+    short int           positionX,
+                        positionY;
+    
+public:
+    // Player Functions:
+                        Player();
+    void                set_position_x(unsigned short int position_x),
+                        set_up_key(sf::Keyboard::Key up_key),
+                        set_down_key(sf::Keyboard::Key down_key),
+                        move(sf::Time timeReference);
+    sf::RectangleShape  get_paddle();
+};
+
+#endif
