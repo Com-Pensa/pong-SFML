1250c4b6ba472e0660409ed707c593426c9a65d8
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-05-26 19:25:48 -0300

    Create Game.hpp

---
+++ src/components/Game.hpp
@@ -0,0 +1,36 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+//  Game objects
+#include "obj/Player.hpp"
+#include "obj/Ball.hpp"
+
+class Game
+{   
+private:
+    //  Game Data and variables
+    sf::RenderWindow   *mainWindow;
+    sf::Event           mainWindowEvent;
+    sf::Clock           mainClock;
+    sf::Time            movimentTime;
+    sf::RectangleShape  middleLine;
+    Player             *rightPlayer,
+                       *leftPlayer;
+    Ball               *mainBall;
+    
+    //  Initializers
+    void               init_window(),
+                       init_middle_line(),
+                       init_players(),
+                       init_ball();
+    
+public:
+    //  Game Functions:
+                        Game();
+    void                run(),
+                        process_events(),
+                        process_live_events(),
+                        show();
+};
+
+#endif
