e1ac7b8da373d9cd7c3c84f359f87975ac7bbfae
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-05-28 15:06:02 -0300

    Compatibility with Constants.hpp

--- src/components/obj/Ball.cpp
+++ src/components/obj/Ball.cpp
@@ -4,13 +4,15 @@ Ball::Ball()
 {   
     goingRight=true, goingDown=true;
     
-    ballCircle.setRadius(10);
+    ballCircle.setRadius(ballRadius);
     to_center();
 }
 
 void Ball::to_center()
 {   
-    positionX=590, positionY=327;  // Updating the data
+    positionX=window[0]/2 - ballRadius;
+    positionY=window[1]/2 - ballRadius;
+    
     goingRight = (goingRight) ? false : true; // Invert
     ballCircle.setPosition(sf::Vector2f(positionX, positionY));
 }
@@ -18,11 +20,11 @@ void Ball::to_center()
 void Ball::move(sf::Time timeReference)
 {   
     //  Converting the time to an integer:
-    short moviment = 0.55 /* Speed */ * timeReference.asMilliseconds();
+    short moviment = ballVelocity * timeReference.asMilliseconds();
     
     //  Validating moviments in Y axis:
-    if (positionY-moviment<0)          goingDown=true;
-    else if (positionY+moviment>=655)  goingDown=false;
+    if (positionY-moviment<0)                   goingDown=true;
+    else if (positionY+moviment>=(window[1]-20))goingDown=false;
     
     //  Calculating the positionY:
     if (goingDown) positionY += moviment;
@@ -40,12 +42,12 @@ void Ball::make_colision(sf::RectangleShape playerPaddle)
     // If The ball collides with the paddle:
     if (ballCircle.getGlobalBounds().intersects(playerPaddle.getGlobalBounds()))
     {   
-        if (positionX<597)    goingRight=true;
-        else                  goingRight=false;
+        if (positionX<middleLinePosition[0]) goingRight=true;
+        else                                 goingRight=false;
     }
     
     // If the ball reach the limits in X of the sides window:
-    if (positionX <= -80 || positionX >= 1300)
+    if (positionX <= -100 || positionX >= (window[0]+80))
     {   
         to_center();
     }
